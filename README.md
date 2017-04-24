8c2482326251c480159345f1aaf2ef4c689b6bf4
codeofsea <goodsoftware@yandex.com>
2017-04-24 18:51:10 +1000

    New verison released at 2017 04 24

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 The jQuery directory plugin
 
 This is first version, we are adding all things now, description, guide, F.A.Q are coming soon! :)
+
+New version be released at 2017 04 24, example and detailed how to use is comming soon. :)
--- jquery.directory.js
+++ jquery.directory.js
@@ -1,15 +1,14 @@
 /*!
  * jQuery.Directory. The jQuery directory plugin
  *
- * Copyright (c) 2014 Tomas Zhu
+ * Copyright (c) 2014 - 2017 Tomas Zhu
  * http://tomas.zhu.bz
  * Support: http://tomas.zhu.bz/jquery-directory-plugin.html
- * License: GPLv3 or later
- * https://www.gnu.org/licenses/gpl-3.0.html
- * https://www.gnu.org/licenses/quick-guide-gplv3.html
- * This program comes with ABSOLUTELY NO WARRANTY;
+ * Licensed under GPLv3 licenses
+ * http://www.gnu.org/licenses/gpl.html
+ *
  * Launch  : June 2014
- * Version : 1.1.0
+ * Version : 2.0.0
  * Released: 10 June, 2014 - 00:00
  * 
  */
@@ -17,47 +16,82 @@
 {
 	$.fn.directory = function(options)
 	{
-		var defaults = {
-				navigation: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-				frontground: 'red',    
-    			background: 'yellow'    // moved color to directory.css 
-  			};
-		var opts = $.extend(defaults, options);
-		return this.each(function () {
+		if ( options['language'] == 'sv' ) 
+		{
+			$.fn.directory.defaults.navigation = 
+				['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'š', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ž', 'å', 'ä', 'ö'] 
 			
-			$currentthis = $(this);
-			$($currentthis).find('span').each(function()
+		};
+		if ( options['language'] == 'en' )
+		{
+			$.fn.directory.defaults.navigation = 
+				['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']								
+		}
+		if ( options['language'] == 'de' )
+		{
+			$.fn.directory.defaults.navigation = 
+				['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü', 'ß']								
+		}
+		if ( options['language'] == 'fr' )
+		{
+			$.fn.directory.defaults.navigation = 
+				['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'é', 'è', 'ç', 'ë', 'ò', 'ô', 'ö', 'ù', 'à', 'â']								
+		}
+		if ( options['language'] == 'es' )
+		{
+			$.fn.directory.defaults.navigation = 
+				['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'll','m', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']								
+		}
+		if ( options['language'] == 'fi' )
+		{
+			$.fn.directory.defaults.navigation = 
+				['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'š', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ž', 'å', 'ä', 'ö']								
+		}
+		
+		var opts = $.extend($.fn.directory.defaults, options || {});
+
+		var tooltipsShowNav = function(tooltipsNavBar)
+		{
+
+			
+			var currentThis = $(tooltipsNavBar);
+
+			$(currentThis).find('span').each(function()
 			{
-				var currentstring = $.trim($(this).text()).toLowerCase();
-				var firstAlpha = currentstring.charAt(0);
+				var currentString = $.trim($(this).text()).toLowerCase();
+				var firstAlpha = currentString.charAt(0);
+
 				$(this).data('alpha',firstAlpha);
 			});
+			
+			
 			var navbar = '<a class="navitem allDirectory" href="#">ALL</a>';
-			$.each(defaults.navigation, function(i,val)
+			$.each($.fn.directory.defaults.navigation, function(i,val)
 			{
-				//navbar = navbar + '<a class="navitem '+val+'" href="#">'+val.toUpperCase()+'</a>';
-				var ttt = $('.tooltips_list > span');
-				var countstore = 0;
+				var directorySelectors = $.fn.directory.defaults.selectors
+				var ttt = $(directorySelectors);
+				var countStore = 0;
 				$.each(ttt,function()
 				{
-					$nowval = $(this);
-					var alphacount = $($nowval).data('alpha');
+					var nowVal = $(this);
+					var alphacount = $(nowVal).data('alpha');
 					if (val == alphacount)
 					{
-						countstore = countstore + 1;
+						countStore = countStore + 1;
 					}
 				}
 				)
-				if (countstore == 0 ) 
+				if (countStore == 0 ) 
 				{
-					navbar = navbar + '<a class="navitem '+val+'" data-counter='+countstore+' href="#">'+val.toUpperCase()+'</a>';	
+					navbar = navbar + '<a class="navitem '+val+'" data-counter='+countStore+' href="#">'+val.toUpperCase()+'</a>';	
 				}
 				else {
-				navbar = navbar + '<a class="navitem '+val+' href="#">'+val.toUpperCase()+'<span class="tooltiplist_count">'+countstore+'</span></a>';
+				navbar = navbar + '<a class="navitem '+val+' href="#">'+val.toUpperCase()+'<span class="tooltiplist_count">'+countStore+'</span></a>';
 				}
   			});
   			navbar = '<div class="navitems">' + navbar + '</div>';
-			$currentthis.prepend(navbar);
+			currentThis.prepend(navbar);
+
 			$('.navitem').css('color','#007DBD');
 			$('.navitem').click(function()
 			{
@@ -67,15 +101,15 @@
 				$(this).css('background','#007DBD');
 				$('.navitem').css('color','#007DBD');
 				$(this).css('color','#fff');
-				$currentcheck = $(this);
-				$clickedAlpha = $.trim($(this).text()).toLowerCase();
-				$clickedAlphaFirst = $clickedAlpha.charAt(0);
+				var currentCheck = $(this);
+				var clickedAlpha = $.trim(currentCheck.text()).toLowerCase();
+				var clickedAlphaFirst = clickedAlpha.charAt(0);
 
-				$($currentthis).find('span').each(function()
+				$(currentThis).find('span').each(function()
 				{
 					var alpha = $(this).data('alpha');
 					
-					if ($clickedAlphaFirst == alpha)
+					if (clickedAlphaFirst == alpha)
 					{
 						$(this).css('display','inline-block');
 						$('.tooltiplist_count').css('display','inline-block');
@@ -86,12 +120,27 @@
 						$('.tooltiplist_count').css('display','inline-block');
 					}
 
-					if ($clickedAlpha == 'all')
+					if (clickedAlpha == 'all')
 					{
 						$(this).css('display','inline-block');
 					}
 				});
-  			});
+  			});			
+		}		
+		
+		var tooltipsResults = this.each(function () 
+		{
+			tooltipsShowNav(this);
 		});
+		return tooltipsResults;
+		
    };
+   
+   $.fn.directory.defaults = {
+			navigation: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],				
+			frontground: 'red',    
+			background: 'yellow',    // moved color to directory.css
+			methods:	'list',
+			selectors:	'.tooltips_list > span'
+			};
 })(jQuery);
